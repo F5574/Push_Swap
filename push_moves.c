ef56e842968cb91410dee8541eb0ff4979bad556
F5574 <gabrielvonah04@outlook.com>
2025-02-18 17:45:33 +0000

    update sort

--- push_moves.c
+++ push_moves.c
@@ -1,16 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_sortbig_2.c                                   :+:      :+:    :+:   */
+/*   push_moves.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gvon-ah- <gvon-ah-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 19:51:14 by gvon-ah-          #+#    #+#             */
-/*   Updated: 2025/02/13 15:30:38 by gvon-ah-         ###   ########.fr       */
+/*   Updated: 2025/02/18 17:45:07 by gvon-ah-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h";
+
+static void rotate_both(t_stack **b, t_stack **a, t_stack *cheap, bool rr)
+{
+	while (*b != cheap->target && *a != cheap)
+	{
+		if (rr)
+			double_reverse_rotate(a,b);
+		else
+			double_rotate(a,b);
+	}
+	current_index(*a);
+	current_index(*b);
+}
+
+static void move_to_b(t_stack **a, t_stack **b)
+{
+	t_stack	*c_node;
+
+	c_node = get_cheapest(*a);
+	if (c_node->above_median && c_node->target->above_median)
+		rotate_both(a, b, c_node, 0);
+	else if (!(c_node->above_median) && !(c_node->target->above_median))
+		rotate_both(a, b, c_node, 1);
+	prep_for_push(a, c_node, 'a');
+	prep_for_push(b, c_node, 'b');
+	push_to_stack(a,b, 'b');
+}
+
+static void move_to_a(t_stack **a, t_stack **b)
+{
+	prep_for_push(a, (*b)->target, 'a');
+	push_to_stack(b,a, 'a');
+}
+
+static void min_top(t_stack **stack)
+{
+	while((*stack)->num != min_n(*stack)->num)
+	{
+		if (min_n(*a)->above_median)
+			rotate_stack(stack, 'a' ,0);
+		else
+			reverse_rotate_stack(stack, 'a', 0);
+	}
+}
 
 void	sort_stacks(t_stack **a, t_stack **b)
 {
@@ -34,15 +78,4 @@ void	sort_stacks(t_stack **a, t_stack **b)
 	}
 	current_index(*a);
 	min_top(a);
-}
-static void move_to_b(t_stack **a, t_stack **b)
-{
-	t_stack	*c_node;
-
-	c_node = get_cheapest(*a);
-	if (c_node->above_median && c_node->target->above_median)
-		double_rotate(a, b);
-	else if (!(c_node->above_median) && !(c_node->target->above_median))
-		double_reverse_rotate(a, b);
-	
 }
--- push_sort.c
+++ push_sort.c
@@ -1,34 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_prep.c                                        :+:      :+:    :+:   */
+/*   push_sort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gvon-ah- <gvon-ah-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/13 18:55:00 by gvon-ah-          #+#    #+#             */
-/*   Updated: 2025/02/18 17:04:05 by gvon-ah-         ###   ########.fr       */
+/*   Created: 2025/02/18 17:05:14 by gvon-ah-          #+#    #+#             */
+/*   Updated: 2025/02/18 17:25:51 by gvon-ah-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"push_swap.h"
+#include "push_swap.h"
 
-void	prep_for_push(t_stack **stack, t_stack *top, char c)
-{
-	while (*stack != top)
-	{
-		if (c == 'a')
-		{
-			if (top->above_median)
-				rotate_stack(stack, 'a', 0);
-			else
-				reverse_rotate_stack(stack, 'a', 0);	
-		}
-		else
-		{
-			if (top->above_median)
-				rotate_stack(stack, 'b', 0);
-			else
-				reverse_rotate_stack(stack, 'b', 0);
-		}
-	}
-}
+
